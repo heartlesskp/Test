716b1e8b81249c7d623ff73dec454b10a8778f95
KP <panchalkaushal1234@gmail.com>
2020-06-28 22:20:49 +0530

    country issue solved.

--- app/src/main/java/com/expertbrains/startegictest/widgets/CountryRepository.kt
+++ app/src/main/java/com/expertbrains/startegictest/widgets/CountryRepository.kt
@@ -8,6 +8,7 @@ object CountryRepository {
     val all: ArrayList<CountryItem>
         get() {
             val countries: ArrayList<CountryItem> = ArrayList()
+            val countriesTemp: ArrayList<String> = ArrayList()
             val locales: Array<Locale> = Locale.getAvailableLocales()
             for (locale in locales) {
                 val countryCode: String = locale.country
@@ -15,7 +16,10 @@ object CountryRepository {
                 if (countryCode.trim { it <= ' ' }.isNotEmpty() && countryName.trim { it <= ' ' }
                         .isNotEmpty()) {
                     val country = CountryItem(countryCode, countryName)
-                    countries.add(country)
+                    if (countriesTemp.contains(countryName).not()) {
+                        countriesTemp.add(countryName)
+                        countries.add(country)
+                    }
                 }
             }
             return countries
